32d59a7f847555b64629b916a57cae0ff87e6446
charles drolet <cdrolet@expedia.com>
2016-04-16 13:02:39 -0400

    test

--- src/main/java/org/cdrolet/cdirect/dto/EventDetail.java
+++ src/main/java/org/cdrolet/cdirect/dto/EventDetail.java
@@ -3,6 +3,7 @@ package org.cdrolet.cdirect.dto;
 import lombok.Data;
 
 import java.io.Serializable;
+import org.cdrolet.cdirect.request.RequestUtil;
 
 @Data
 public class EventDetail implements Serializable {
@@ -14,4 +15,9 @@ public class EventDetail implements Serializable {
     private EventCreator creator;
     private EventPayload payload;
     private String returnUrl;
+
+
+    public static EventDetail fromJson(String json) {
+        return RequestUtil.fromJson(json, EventDetail.class);
+    }
 }
---
+++ src/test/java/org/cdrolet/cdirect/request/RequestUtilTest.java
@@ -0,0 +1,18 @@
+package org.cdrolet.cdirect.request;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RequestUtilTest {
+
+    @Test
+    public void testFromJson() throws Exception {
+
+    }
+
+    @Test
+    public void testValuesToMap() throws Exception {
+
+    }
+}
