8b196fcd8173b87eada19e713fd1344696d80e0c
Charles D <charles.drokar@gmail.com>
2016-04-16 14:32:40 -0400

    fixing typo

--- src/main/java/org/cdrolet/cdirect/dto/EventResult.java
+++ src/main/java/org/cdrolet/cdirect/dto/EventResult.java
@@ -1,12 +1,13 @@
 package org.cdrolet.cdirect.dto;
 
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 
 import java.io.Serializable;
 
-@Getter
 @Builder
+@Data
 public class EventResult implements Serializable {
     private static final long serialVersionUID = -7599199539526987847L;
 
