8e67416f71cafcdc282c43100723f7733db81f5d
Charles D <charles.drokar@gmail.com>
2016-04-16 13:26:53 -0400

    adding json unit tests

--- src/test/java/org/cdrolet/cdirect/dto/EventDetailTest.java
+++ src/test/java/org/cdrolet/cdirect/dto/EventDetailTest.java
@@ -1,54 +1,28 @@
 package org.cdrolet.cdirect.dto;
 
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class EventDetailTest {
 
+    private final Gson gson = new Gson();
+
     @Test
-    public void fromJson() throws Exception {
-        String sample = " {" +
-                "'type':'SUBSCRIPTION_ORDER'," +
-                "'marketplace':{'partner':'ACME','baseUrl':'https://acme.appdirect.com'}," +
-                "'applicationUuid':null," +
-                "'flag':'STATELESS', " +
-                "'creator':{" +
-                "'uuid':'ec5d8eda-5cec-444d-9e30-125b6e4b67e2'," +
-                "'openId':'https://www.appdirect.com/openid/id/ec5d8eda-5cec-444d-9e30-125b6e4b67e2'," +
-                "'email':'test-email+creator@appdirect.com'," +
-                "'firstName':'DummyCreatorFirst'," +
-                "'lastName':'DummyCreatorLast'," +
-                "'language':'fr'," +
-                "'address':null," +
-                "'attributes':null" +
-                "}," +
-                "'payload':{" +
-                "'user':null," +
-                "'company':{'uuid':'d15bb36e-5fb5-11e0-8c3c-00262d2cda03'," +
-                "'externalId':null," +
-                "'name':'Example Company Name'," +
-                "'email':'company-email@example.com'," +
-                "'phoneNumber':'415-555-1212'," +
-                "'website':'http://www.example.com'," +
-                "'country':'CA'" +
-                "}," +
-                "'account':null,'addonInstance':null," +
-                "'addonBinding':null," +
-                "'order':{" +
-                "'editionCode':'BASIC'," +
-                "'addonOfferingCode':null," +
-                "'pricingDuration':'MONTHLY'," +
-                "'items':[{'unit':'USER'," +
-                "'quantity':10}," +
-                "{'unit':'MEGABYTE'," +
-                "'quantity':15}]}," +
-                "'notice':null," +
-                "'configuration':{" +
-                "'domain':'" +
-                "mydomain'}}," +
-                "'returnUrl':'https://www.appdirect.com/finishprocure?token=dummyOrder'," +
-                "'links':[]}";
-        assertThat(EventDetail.fromJson(sample)).isNotNull();
+    public void eventDetail_can_be_parsed_from_subscription_order() throws Exception {
+
+        EventDetail detail = gson.fromJson(readerOf("./target/test-classes/subscriptionOrder.json"), EventDetail.class);
+
+        assertThat(detail).isNotNull();
+    }
+
+    private JsonReader readerOf(String fileName) throws FileNotFoundException {
+        return new JsonReader(new FileReader(fileName));
     }
 }
