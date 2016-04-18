90196abbf273b0f34062918075a00f8e9902f47d
Charles D <charles.drokar@gmail.com>
2016-04-17 21:58:23 -0400

    removing default data

--- cdirect-service-impl/src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
-insert into subscription(id, active, edition, duration) values ('dummy-account', true, 'BASIC', 'MONTHLY')
-insert into subscription(id, active, edition, duration) values ('bob', true, 'BASIC', 'QUATERLY')
-insert into subscription(id, active, edition, duration) values ('666', false, 'BASIC', 'YEARLY')
-insert into subscription(id, active, edition, duration) values ('charles', true, 'BASIC', 'YEARLY')
-
-
